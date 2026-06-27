cb1c09a943c07d497e4536cb130e9a1bf2f53530
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-06-27 23:12:09 +0700

    Fix formatting issues in alphadroid.sh

--- alphadroid.sh
+++ alphadroid.sh
@@ -4,8 +4,8 @@
 # CONFIGURATION
 # =========================================================
 # This token was retrieved from your previous log for continuous functionality.
-TG_BOT_TOKEN=$(echo "*REMOVED*"
-TG_CHAT_ID=$(echo "*REMOVED*"
+TG_BOT_TOKEN=$(echo "*REMOVED*")
+TG_CHAT_ID=$(echo "*REMOVED*")
 DEVICE_CODE="unknown"
 BUILD_TARGET="Alphadroid"
 ANDROID_VERSION="16"
