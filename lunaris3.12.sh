0b6852d902247fb1a2b8178e83e83828766f3a10
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-08-17 12:58:00 +0700

    Change device repository and maintainer details
    Updated device repository source for cloning and modified maintainer information in system.prop.

--- lunaris3.12.sh
+++ lunaris3.12.sh
@@ -142,14 +142,12 @@ start_build_process() {
 
     echo "Cloning device trees..."
     git clone https://github.com/aoitsme/android_kernel_sony_sdm845 -b bpf --depth=1 kernel/sony/sdm845
-    git clone https://github.com/aoitsme/android_device_sony_"$DEVICE_CODE" -b lunaris-16.2 --depth=1 device/sony/"$DEVICE_CODE"
+    git clone https://github.com/ganendra4u/android_device_sony_"$DEVICE_CODE" -b lunaris-16.2 --depth=1 device/sony/"$DEVICE_CODE"
     git clone https://github.com/aoitsme/android_device_sony_tama-common -b lineage-23.2 --depth=1 device/sony/tama-common
     git clone https://github.com/aoitsme/android_hardware_sony_SonyOpenTelephony -b lineage-23.2 --depth=1 hardware/sony/SonyOpenTelephony
     git clone https://github.com/aoitsme/proprietary_vendor_sony_"$DEVICE_CODE" -b lineage-23.2 --depth=1 vendor/sony/"$DEVICE_CODE"
     git clone https://github.com/aoitsme/proprietary_vendor_sony_tama-common -b lineage-23.2 --depth=1 vendor/sony/tama-common
     git clone https://github.com/aoitsme/keys -b master --depth=1 vendor/lineage-priv
-
-sed -i 's/^ro.lunaris.maintainer=.*/ro.lunaris.maintainer=Ganendra1945 | ganx0000/' device/sony/"$DEVICE_CODE"/system.prop
 
 cat >> device/sony/apollo/lineage_apollo.mk << 'EOF'
 USE_REALITY_ENGINE := true
