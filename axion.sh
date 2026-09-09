498a34bcac4b2a7402801faf5063d0817001831b
GanX <daffaofficial224@gmail.com>
2026-09-09 21:40:49 +0700

    Remove repository replacement for kernel configs and interfaces
    Removed repository replacement commands for kernel configs and hardware interfaces.

--- axion.sh
+++ axion.sh
@@ -136,12 +136,6 @@ start_build_process() {
     fi
     repo sync
     
-    echo "Replacing some repository..."
-    rm -rf kernel/configs
-    rm -rf hardware/interfaces
-    git clone https://github.com/crdroidandroid/android_kernel_configs -b 16.0 kernel/configs --depth=1
-    git clone https://github.com/crdroidandroid/android_hardware_interfaces -b 16.0 hardware/interfaces --depth=1
-
     echo "Patch frameroks_native..."
     cd frameworks/native
     wget https://raw.githubusercontent.com/aoitsme/crave_script/refs/heads/main/patch/001-temp-fix-camera.patch
